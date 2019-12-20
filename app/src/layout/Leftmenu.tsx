ffe9b624760070b8164760c5a9e40708a311d3bb
liuskr <1728524385@qq.com>
2019-12-20 09:50:37 +0800

    修改router

--- app/src/layout/Leftmenu.tsx
+++ app/src/layout/Leftmenu.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Leftmenu: React.FC = () => {
   return (
     <div className="menu">
-      
+       <div>
+           hello word
+       </div>
     </div>
   )
 };
