c7d971dacab5c560264fa480b9f4f1a05d1ba477
liuskr <1728524385@qq.com>
2019-12-24 14:09:55 +0800

    类型错误

--- app/src/pages/discover/Discover.tsx
+++ app/src/pages/discover/Discover.tsx
@@ -1,31 +1,77 @@
-import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
 import './style/tab.less';
 import Recommend from './Recommend';
 import SongList from './SongList';
 
-const Discover: React.FC = (props) => (
-  <div className="l-disbox">
-    <ul className="l-ul">
-      <li>
-        <Link to="/discover/recommend">个性推荐</Link>
-      </li>
-      <li>
-        <Link to="/discover/songlist">歌单</Link>
-      </li>
-      <li>主播电梯</li>
-      <li>排行榜</li>
-      <li>歌手</li>
-      <li>最新音乐</li>
-    </ul>
-    <div>
-      {props.children}
-      <Switch>
-        <Route path="/discover/recommend" component={Recommend} />
-        <Route path="/discover/songlist" component={SongList} />
-      </Switch>
-    </div>
-  </div>
-);
+interface listData { 
+      url:string,
+      name:string 
+    }
+ interface State {
+  list: Array<object>;
+}
+
+ interface Props {
+   home: string;
+}
+
+class Discover extends React.Component<Props, State> {
+  constructor(props:Props) {
+    super(props);
+    
+    this.state = {
+      list: [
+        {
+          path:'/discover/recommend',
+          name:'个性推荐'
+        },
+        {
+          path:'/discover/recommend',
+          name:'个性推荐'
+        },
+        {
+          path:'/discover/recommend',
+          name:'个性推荐'
+        },
+        {
+          path:'/discover/recommend',
+          name:'个性推荐'
+        }
+      ]
+    };
+  }
+  public render() {
+    return (
+      <div className="l-disbox">
+        <ul className="l-ul">
+          {this.state.list.map((item,index)=>{
+            console.log(item)
+              return <li>
+                     <Link to={item.path}>{item.name}</Link>
+                    </li>
+          })}
+          {/* <li>
+            <Link to="/discover/recommend">个性推荐</Link>
+          </li>
+          <li>
+            <Link to="/discover/songlist">歌单</Link>
+          </li>
+          <li>主播电梯</li>
+          <li>排行榜</li>
+          <li>歌手</li>
+          <li>最新音乐</li> */}
+        </ul>
+        <div>
+          {this.props.children}
+          <Switch>
+            <Route path="/discover/recommend" component={Recommend} />
+            <Route path="/discover/songlist" component={SongList} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Discover;
