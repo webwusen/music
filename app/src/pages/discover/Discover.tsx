6055fa43529fcdcc050cd8a13f61e19b090943bf
liuskr <1728524385@qq.com>
2019-12-24 15:18:56 +0800

    tab栏

--- app/src/pages/discover/Discover.tsx
+++ app/src/pages/discover/Discover.tsx
@@ -1,66 +1,65 @@
-import React, { Component } from 'react';
-import { Route, Link, Switch, Redirect } from 'react-router-dom';
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
 import './style/tab.less';
 import Recommend from './Recommend';
 import SongList from './SongList';
 
-interface listData { 
-      url:string,
-      name:string 
-    }
- interface State {
-  list: Array<object>;
+interface listData {
+  path: string;
+  name: string;
+  id: number;
+}
+interface State {
+  list: Array<listData>;
 }
 
- interface Props {
-   home: string;
+interface Props {
+  home: string;
 }
 
 class Discover extends React.Component<Props, State> {
-  constructor(props:Props) {
+  constructor(props: Props) {
     super(props);
-    
     this.state = {
       list: [
         {
-          path:'/discover/recommend',
-          name:'个性推荐'
+          path: '/discover/recommend',
+          name: '个性推荐',
+          id: 1
         },
         {
-          path:'/discover/recommend',
-          name:'个性推荐'
+          path: '/discover/songlist',
+          name: '歌单',
+          id: 2
         },
         {
-          path:'/discover/recommend',
-          name:'个性推荐'
+          path: '/discover/recommendsss',
+          name: '主播电台',
+          id: 3
         },
         {
-          path:'/discover/recommend',
-          name:'个性推荐'
+          path: '/discover/recommendss',
+          name: '歌手',
+          id: 4
+        },
+        {
+          path: '/discover/recommends',
+          name: '最新音乐',
+          id: 5
         }
       ]
     };
   }
   public render() {
+    const menu = this.state.list.map((item, index) => {
+      <li key={index}>
+        <Link to={item.path}>{item.name}</Link>
+      </li>;
+    });
     return (
       <div className="l-disbox">
         <ul className="l-ul">
-          {this.state.list.map((item,index)=>{
-            console.log(item)
-              return <li>
-                     <Link to={item.path}>{item.name}</Link>
-                    </li>
-          })}
-          {/* <li>
-            <Link to="/discover/recommend">个性推荐</Link>
-          </li>
-          <li>
-            <Link to="/discover/songlist">歌单</Link>
-          </li>
-          <li>主播电梯</li>
-          <li>排行榜</li>
-          <li>歌手</li>
-          <li>最新音乐</li> */}
+           {menu}       
         </ul>
         <div>
           {this.props.children}
