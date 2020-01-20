864be1dc139464a7aeeccb3de0653fb806482e7e
webwusen <898036454@qq.com>
2020-01-20 13:43:11 +0800

    discover使用hooks编写

--- app/src/pages/discover/Discover.tsx
+++ app/src/pages/discover/Discover.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Link, Switch, Redirect } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Link, Switch, Redirect, withRouter } from 'react-router-dom';
 import './style/tab.less';
 import Recommend from './Recommend';
 import SongList from './SongList';
@@ -8,75 +8,60 @@ interface listData {
   path: string;
   name: string;
 }
-interface State {
-  list: Array<listData>;
-  active: number;
-}
 
 interface Props {
-  home: string;
+  [propName: string]: any;
 }
 
-class Discover extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      list: [
-        {
-          path: '/discover/recommend',
-          name: '个性推荐'
-        },
-        {
-          path: '/discover/songlist',
-          name: '歌单'
-        },
-        {
-          path: '/discover/recommend',
-          name: '主播电台'
-        },
-        {
-          path: '/discover/recommend',
-          name: '歌手'
-        },
-        {
-          path: '/discover/recommend',
-          name: '最新音乐'
-        }
-      ],
-      active: 0
-    };
+const list: Array<listData> = [
+  {
+    path: '/discover/recommend',
+    name: '个性推荐'
+  },
+  {
+    path: '/discover/songlist',
+    name: '歌单'
+  },
+  {
+    path: '/discover/station',
+    name: '主播电台'
+  },
+  {
+    path: '/discover/singer',
+    name: '歌手'
+  },
+  {
+    path: '/discover/lastestmusic',
+    name: '最新音乐'
   }
-  switchClass = (index: any) => {
-    this.setState({
-      active: index
-    });
-  };
-  render() {
-    const menu = this.state.list.map((item, index) => (
-      <li
-        key={index}
-        className={index === this.state.active ? 'l-active' : undefined}
-        onClick={() => this.switchClass(index)}
-      >
-        <Link className={index === this.state.active ? 'l-textc' : undefined} to={item.path}>
-          {item.name}
-        </Link>
-      </li>
-    ));
-    return (
-      <div className="l-disbox">
-        <ul className="l-ul">{menu}</ul>
-        <div>
-          {this.props.children}
-          <Switch>
-            <Route path="/discover/recommend" component={Recommend} />
-            <Route path="/discover/songlist" component={SongList} />
-            <Redirect exact from="/discover" to="/discover/recommend" />
-          </Switch>
-        </div>
+];
+const Discover: React.FC = (props: Props) => {
+  const pathname: string = props.location.pathname.split('/');
+  const [active, setActive] = useState(`/${pathname[1]}/${pathname[2]}`);
+  const menu = list.map((item, index) => (
+    <li
+      key={item.path}
+      className={item.path === active ? 'l-active' : undefined}
+      onClick={() => setActive(item.path)}
+    >
+      <Link className={item.path === active ? 'l-textc' : undefined} to={item.path}>
+        {item.name}
+      </Link>
+    </li>
+  ));
+  return (
+    <div className="l-disbox">
+      <ul className="l-ul">{menu}</ul>
+      <div>
+        {props.children}
+        <Switch>
+          <Route path="/discover/recommend" component={Recommend} />
+          <Route path="/discover/songlist" component={SongList} />
+          <Redirect exact from="/discover" to="/discover/recommend" />
+        </Switch>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default Discover;
+export default withRouter(Discover);
